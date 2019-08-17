21e249bad02bb2e767a3b1993756556c831e801b
Tony <alongsmile@126.com>
2019-08-17 13:17:41 +0800

    Initial commit

---
+++ huihui/src/main/java/com/huihui/core/Utils.java
@@ -0,0 +1,11 @@
+package com.huihui.core;
+
+/**
+* @Author huihui 
+* @Date 2019/8/17
+* @Description 
+*/
+public class Utils {
+
+
+}
---
+++ src/TestHello.java
@@ -0,0 +1,14 @@
+import jdk.nashorn.internal.runtime.linker.LinkerCallSite;
+
+import java.awt.*;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class TestHello {
+    public static void main(String[] args) {
+
+    }
+}
