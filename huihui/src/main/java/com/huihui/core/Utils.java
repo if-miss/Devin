6dad9e22353cfdfbd5462b5b9bbb8f19484cfce0
huguangjun <huguangjun_bj@163.com>
2019-08-17 13:33:11 +0800

    工具类调整

--- huihui/src/main/java/com/huihui/core/Utils.java
+++ huihui/src/main/java/com/huihui/core/Utils.java
@@ -1,5 +1,8 @@
 package com.huihui.core;
 
+import java.util.List;
+import java.util.TimerTask;
+
 /**
  * @Author huihui
  * @Date 2019/8/17
@@ -10,4 +13,8 @@ public class Utils {
     public String jsonToString() {
         return "";
     }
+    public List jsonToString(String str) {
+        return null;
+    }
+
 }
